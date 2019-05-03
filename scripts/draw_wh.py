4a9967270b22d6266e1dce25fb05066911ed860e
Squidroot2 <hpfoley1@gmail.com>
2019-05-02 22:04:29 -0400

    Fixed Crash when changing your mind after trying to flee

--- scripts/draw_wh.py
+++ scripts/draw_wh.py
@@ -23,10 +23,10 @@ def drawBattleScreen(panes, player, buttons, enemy, player_damage=None, enemy_da
         drawEnemy(panes['center'], enemy)
     drawButtons(buttons)
     
-    if not player_damage == None:
+    if player_damage is not None:
         drawDamageDone(panes['right'], player_damage)
         
-    if not enemy_damage == None:
+    if enemy_damage is not None:
         drawDamageDone(panes['left'], enemy_damage)
 
 def drawHelpScreen(buttons):
